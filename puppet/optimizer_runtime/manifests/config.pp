3ed6518432a65fa1f781c2dc3bce5dc545e32909
Tomer Paz <tomer.paz@concertio.com>
2021-02-07 19:29:05 +0200

    puppet: support OPTIMIZER_DEFINITION_FILE env variable
    from next release of runtime (2.1), this env var shall be used
    to set knobs definition yaml.
    will ease installation via systemd service

--- puppet/optimizer_runtime/manifests/config.pp
+++ puppet/optimizer_runtime/manifests/config.pp
@@ -13,14 +13,25 @@ class optimizer_runtime::config {
       require => Class['optimizer_runtime::install'],
     }
   }
-  # include our built in knobs.yaml just in case...
-  file { $optimizer_runtime::params::config_path:
-    ensure  => file,
-    path    => $optimizer_runtime::params::config_path,
-    content => file('optimizer_runtime/knobs.yaml'),
-    owner   => 0,
-    group   => 0,
-    mode    => '0400',
-    require => Class['optimizer_runtime::install'],
+  else {
+    # include our built in knobs.yaml just in case...
+    file { $optimizer_runtime::params::config_path:
+      ensure  => file,
+      path    => $optimizer_runtime::params::config_path,
+      content => file('optimizer_runtime/knobs.yaml'),
+      owner   => 0,
+      group   => 0,
+      mode    => '0400',
+      require => Class['optimizer_runtime::install'],
+    }
+  }
+
+  file_line { 'optimizer-runtime service knobs configuration':
+    ensure  => present,
+    path    => '/lib/systemd/system/optimizer-runtime.service',
+    line    => "Environment='OPTIMIZER_DEFINITION_FILE=${optimizer_runtime::config_path}'",
+    match   => '^Environment\=',
+    require => File[$optimizer_runtime::config_path],
+    notify  => Class['optimizer_runtime::Service'],
   }
 }
